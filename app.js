8a0d1597ef265c93988a78e1583c9e5a15e40823
duongdv95 <danielvanduong@gmail.com>
2018-04-15 02:51:27 +0000

    add directories: views -> partials

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("test");
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser');
+    
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req,res){
+    res.render("home");
+})
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server has started..")
+})
